619d4c92abc4dea5357e207e034e0147ce6857a7
ghayani GN <91198271+Ghayani1098@users.noreply.github.com>
2022-01-19 10:00:53 +0530

    pythonfiles

---
+++ blank_game.py
@@ -0,0 +1,90 @@
+import pygame
+import sys
+import os
+from pygame.locals import *
+import math
+pygame.init()
+
+
+DISPLAYSURF = pygame.display.set_mode((500,400))
+pygame.display.set_caption('Doggie!')
+FPS= 120
+fpsClock = pygame.time.Clock()
+
+#colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+sky_blue = (0, 180, 230)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+
+#Scenery
+def scenery():
+    DISPLAYSURF.fill(sky_blue)
+    sun = pygame.draw.circle(DISPLAYSURF, yellow, (400, 50), 30, 0)
+    landscape = pygame.draw.rect(DISPLAYSURF, green, (0, 350, 500, 400))
+
+
+dogImg1 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (1).png')
+dogImg1 = pygame.transform.scale(dogImg1, (60, 80))
+dogImg2 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (2).png')
+dogImg2 = pygame.transform.scale(dogImg2, (60, 80))
+dogImg3 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (3).png')
+dogImg3 = pygame.transform.scale(dogImg3, (60, 80))
+dogImg4 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (4).png')
+dogImg4 = pygame.transform.scale(dogImg4, (60, 80))
+dogImg5 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (5).png')
+dogImg5 = pygame.transform.scale(dogImg5, (60, 80)) 
+dogImg6 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (6).png')
+dogImg6 = pygame.transform.scale(dogImg6, (60, 80))
+dogImg7 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (7).png')
+dogImg7 = pygame.transform.scale(dogImg7, (60, 80))
+dogImg8 = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (8).png')
+dogImg8 = pygame.transform.scale(dogImg8, (60, 80))
+
+while True:
+    scenery()
+    for i in range(600):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        a = i
+        DISPLAYSURF.blit(dogImg1, (a ,280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg2, (a, 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg3, (a , 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg4, (a , 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg5, (a , 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg6, (a , 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg7, (a , 280))
+        pygame.display.update()
+        a = a + 1
+        scenery()
+        DISPLAYSURF.blit(dogImg8, (a , 280))
+        pygame.display.update()
+        fpsClock.tick(FPS)
+        a = a + 1
+
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
+
